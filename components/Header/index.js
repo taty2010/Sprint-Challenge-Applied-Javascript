2d40bdf7f2e645b5531547870e9deb74cd2db504
taty2010 <shydalys123@gmail.com>
2019-09-14 14:09:19 -0500

    iterated through each article for the cards

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,26 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header(){
+    const header = document.createElement('div');
+    const date = document.createElement('span');
+    const h1 = document.createElement('h1');
+    const temp = document.createElement('span');
+
+    header.appendChild(date);
+    header.appendChild(h1);
+    header.appendChild(temp);
+
+    header.classList.add('header');
+    date.classList.add('date');
+    temp.classList.add('temp');
+
+    date.textContent = 'March 28, 2019';
+    h1.textContent = 'Lambda Times';
+    temp.textContent = '98 /u00B0';
+
+    return header
+}
+
+const container = document.querySelector('.header-container');
+container.appendChild(Header());
